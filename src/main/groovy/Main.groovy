6db802d670dd170485fc03d7850bb4f92b04418c
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-04-24 15:46:00 +0200

    Gradle issue

---
+++ src/main/groovy/Main.groovy
@@ -0,0 +1,15 @@
+import groovy.transform.CompileStatic
+
+/**
+ *
+ * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
+ */
+
+@CompileStatic
+class Main {
+
+    static void main(String[] args) {
+        new String().sayHello()
+    }
+
+}
